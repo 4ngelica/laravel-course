03d39337dd21b43bd8e44948fd054a4effd6702c
Angélica Batassim <angelicabatassim@gmail.com>
2021-03-16 08:37:35 -0300

    Routes with parameters

--- projects/app_super_gestao/routes/web.php
+++ projects/app_super_gestao/routes/web.php
@@ -13,6 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+//Definindo rotas (que apontam para vistas ou apenas para msgs de callback)
 Route::get('/', 'PrincipalController@principal');
 Route::get('/vista-principal', 'PrincipalController@vistaPrincipal');
 
@@ -21,3 +23,10 @@ Route::get('/vista-sobre-nos', 'SobreNosController@vistaSobreNos');
 
 Route::get('/contato', 'ContatoController@contato');
 Route::get('/vista-contato', 'ContatoController@vistaContato');
+
+//Definindo rotas com parâmetros
+Route::get('/vista-contato/{nome}/{categoria}/{assunto}/{mensagem}',
+  function(string $nome, string $categoria, string $assunto, string $mensagem) {
+    echo "Rota com parâmetro: $nome - $categoria - $assunto - $mensagem";
+  }
+);
