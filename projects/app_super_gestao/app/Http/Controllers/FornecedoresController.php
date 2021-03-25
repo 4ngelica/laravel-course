a7f23fa4efcd968ab617b169b339cf291b4718fc
Angélica Batassim <angelicabatassim@gmail.com>
2021-03-25 12:37:38 -0300

    PHP Ternary

--- projects/app_super_gestao/app/Http/Controllers/FornecedoresController.php
+++ projects/app_super_gestao/app/Http/Controllers/FornecedoresController.php
@@ -7,12 +7,17 @@ use Illuminate\Http\Request;
 class FornecedoresController extends Controller
 {
     public function index(){
+
+      //Estudo da sintaxe da blade
       $fornecedores = ['1 fornecedor'];
 
       $fornecedores2 = [
-        0 => ['nome' => 'Fornecedor 1', 'status' => 'N', 'cnpj' => '12312313124'],
+        0 => ['nome' => 'Fornecedor 1', 'status' => 'N'],
         1 => ['nome' => 'Fornecedor 2', 'status' => 'S', 'cnpj' => '']
       ];
+
+      //Operador ternário
+      echo isset($fornecedores2[0]['cnpj']) ? 'CNPJ informado' : 'CNPJ inexistente';
 
       return view('app.fornecedores', compact('fornecedores'), compact('fornecedores2'));
     }
