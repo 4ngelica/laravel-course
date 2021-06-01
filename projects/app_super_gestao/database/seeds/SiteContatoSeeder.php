1bed8eb76b0a4d1d636e0fa1f5acc5e9cb2ba18a
Angélica Batassim <angelicabatassim@gmail.com>
2021-06-01 08:30:08 -0300

    add site contato factory

--- projects/app_super_gestao/database/seeds/SiteContatoSeeder.php
+++ projects/app_super_gestao/database/seeds/SiteContatoSeeder.php
@@ -12,12 +12,16 @@ class SiteContatoSeeder extends Seeder
      */
     public function run()
     {
-        $contato = new SiteContato();
-        $contato->nome = 'Sistema SG';
-        $contato->telefone = '(11) 88888-8888';
-        $contato->email = 'contato@sg.com.br';
-        $contato->motivo_contato = 1;
-        $contato->mensagem = 'Seja bem vindo ao sistema super gestão!';
-        $contato->save();
+        // $contato = new SiteContato();
+        // $contato->nome = 'Sistema SG';
+        // $contato->telefone = '(11) 88888-8888';
+        // $contato->email = 'contato@sg.com.br';
+        // $contato->motivo_contato = 1;
+        // $contato->mensagem = 'Seja bem vindo ao sistema super gestão!';
+        // $contato->save();
+
+        factory(SiteContato::class, 100)->create();
+
+        // \App\Models\SiteContato::factory()->count(100)->create();
     }
 }
