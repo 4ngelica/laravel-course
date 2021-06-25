e2858703bbd61fa585d2ee9a696b60e6404a8324
Angélica Batassim <angelicabatassim@gmail.com>
2021-06-25 10:40:40 -0300

    add old input in select tag

--- projects/app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
+++ projects/app_super_gestao/resources/views/site/layouts/_components/form_contato.blade.php
@@ -9,9 +9,9 @@
     <br>
     <select class="{{$classe}}" name="motivo_contato">
         <option value="">Qual o motivo do contato?</option>
-        <option value="1">Dúvida</option>
-        <option value="2">Elogio</option>
-        <option value="3">Reclamação</option>
+        <option value="1" {{old('motivo_contato') == 1 ? 'selected' : ''}}>Dúvida</option>
+        <option value="2" {{old('motivo_contato') == 2 ? 'selected' : ''}}>Elogio</option>
+        <option value="3" {{old('motivo_contato') == 3 ? 'selected' : ''}}>Reclamação</option>
     </select>
     <br>
     <textarea name="mensagem" class="{{$classe}}"> {{(old('mensagem') ==! '') ? old('mensagem'): 'Preencha aqui a sua mensagem
