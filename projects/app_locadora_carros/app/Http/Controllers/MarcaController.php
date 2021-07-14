e6eae4932bf9fe665823dbdd693743fb02612430
Angélica Batassim <angelicabatassim@gmail.com>
2021-07-14 14:51:22 -0300

    update logic (put/ patch)

--- projects/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ projects/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -85,7 +85,19 @@ class MarcaController extends Controller
           return response()->json(['erro'=>'Impossível realizar a atualização. O recurso solicitado não existe.'], 404);
         }
 
-        $request->validate($marca->rules(), $marca->feedback());
+        if($request->method() === 'PATCH'){
+          $regrasDinamicas = array();
+
+          foreach ($marca->rules() as $input => $regra) {
+            if(array_key_exists($input, $request->all())){
+              $regrasDinamicas[$input] = $regra;
+            }
+          }
+
+        }else{
+          $request->validate($marca->rules(), $marca->feedback());
+
+        }
         $marca->update($request->all());
         return response()->json($marca, 200);
     }
