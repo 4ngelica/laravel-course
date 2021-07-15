0ef4fdc6bb6a34119c7a820774c46c4e49329eaf
Angélica Batassim <angelicabatassim@gmail.com>
2021-07-15 14:59:36 -0300

    design patterns

--- projects/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ projects/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Repositories\MarcaRepository;
 use App\Models\Marca;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
@@ -19,35 +20,24 @@ class MarcaController extends Controller
      */
     public function index(Request $request)
     {
-      $marcas = array();
-
+      $marcaRepository = new MarcaRepository($this->marca);
       if($request->has('atributos_modelos')){
-        $atributos_modelos = $request->atributos_modelos;
-        $marcas = $this->marca->with('modelos:id,'. $atributos_modelos);
+        $atributos_modelos = 'modelos:id,'.$request->atributos_modelos;
+        $marcaRepository->selectAtributosRgistrosRelacionados($atributos_modelos);
       }else{
         $marcas = $this->marca->with('modelos');
+        $marcaRepository->selectAtributosRgistrosRelacionados('modelos');
       }
-      if($request->has('filtro')){
-        $filtros = explode(';',$request->filtro);
-        foreach ($filtros as $key => $condicao) {
-          $c = explode(':',$condicao);
-          $marcas = $marcas->where($c[0], $c[1], $c[2]);
-        }
 
+      if($request->has('filtro')){
+        $marcaRepository->filtro($request->filtro);
       }
 
       if($request->has('atributos')){
-        $atributos = $request->atributos;
-        $marcas = $marcas->selectRaw($atributos)->get();
-      }else{
-        $marcas = $marcas->get();
+        $marcaRepository->selectAtributos($request->atributos);
       }
-      return response()->json($marcas,200);
-    }
-
-
-  
-
+      return response()->json($marcaRepository->getResultado(),200);
+}
     /**
      * Store a newly created resource in storage.
      *
