4d6b3c159d722d402584dbf5422ed73a9a0ef744
Angélica Batassim <angelicabatassim@gmail.com>
2021-07-15 07:38:55 -0300

    modelocontroller

--- projects/app_locadora_carros/app/Http/Controllers/ModeloController.php
+++ projects/app_locadora_carros/app/Http/Controllers/ModeloController.php
@@ -7,6 +7,9 @@ use Illuminate\Http\Request;
 
 class ModeloController extends Controller
 {
+    public function __construct(Modelo $modelo){
+      $this->modelo = $modelo;
+    }
     /**
      * Display a listing of the resource.
      *
@@ -14,7 +17,7 @@ class ModeloController extends Controller
      */
     public function index()
     {
-        //
+      return response()->json($this->modelo->all(), 200);
     }
 
     /**
@@ -35,7 +38,24 @@ class ModeloController extends Controller
      */
     public function store(Request $request)
     {
-        //
+      $request->validate($this->modelo->rules(), $this->modelo->feedback());
+      $image = $request->file('imagem');
+      $imagem_urn = $image->store('imagens/modelos','public');
+
+      $modelo = $this->modelo->create([
+        'nome' => $request->nome,
+        'imagem' => $imagem_urn,
+        'marca_id' => $request->marca_id,
+        'numero_portas' => $request->numero_portas,
+        'lugares' => $request->lugares,
+        'air_bag' => $request->air_bag,
+        'abs' => $request->abs
+
+      ]);
+
+      $modelo = $this->modelo->create($request->all());
+      return response()->json($modelo, 201);
+
     }
 
     /**
@@ -44,9 +64,14 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function show(Modelo $modelo)
+    public function show($id)
     {
-        //
+      $modelo = $this->modelo->find($id);
+
+      if($modelo ==null){
+        return response()->json(['erro'=>'O recurso solicitado não existe.'], 404);
+      }
+      return response()->json($modelo, 200);
     }
 
     /**
@@ -67,9 +92,46 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Modelo $modelo)
+    public function update(Request $request, $id)
     {
-        //
+      $modelo = $this->modelo->find($id);
+
+      if($modelo ==null){
+        return response()->json(['erro'=>'Impossível realizar a atualização. O recurso solicitado não existe.'], 404);
+      }
+
+      if($request->method() === 'PATCH'){
+        $regrasDinamicas = array();
+
+        foreach ($modelo->rules() as $input => $regra) {
+          if(array_key_exists($input, $request->all())){
+            $regrasDinamicas[$input] = $regra;
+          }
+        }
+
+      }else{
+        $request->validate($modelo->rules());
+
+      }
+
+      if($request->file('imagem')){
+        Storage::disk('public')->delete($modelo->imagem);
+      }
+
+      $image = $request->file('imagem');
+      $imagem_urn = $image->store('imagens/modelos','public');
+
+      $modelo->update([
+        'nome' => $request->nome,
+        'imagem' => $imagem_urn,
+        'marca_id' => $request->marca_id,
+        'numero_portas' => $request->numero_portas,
+        'lugares' => $request->lugares,
+        'air_bag' => $request->air_bag,
+        'abs' => $request->abs
+      ]);
+
+      return response()->json($modelo, 200);
     }
 
     /**
@@ -78,8 +140,17 @@ class ModeloController extends Controller
      * @param  \App\Models\Modelo  $modelo
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Modelo $modelo)
+    public function destroy($id)
     {
-        //
+      $modelo = $this->modelo->find($id);
+
+      if($modelo ==null){
+        return response()->json(['erro'=>'Impossível realizar a exclusão. O recurso solicitado não existe.'], 404);
+      }
+
+        Storage::disk('public')->delete($modelo->imagem);
+
+      $modelo->delete();
+      return response()->json(['msg' => 'O modelo foi removida com sucesso.'], 200);
     }
 }
