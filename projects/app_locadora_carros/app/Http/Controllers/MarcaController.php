72de59d59fcd723ed15a4a56da2e346b627b26b2
Angélica Batassim <angelicabatassim@gmail.com>
2021-07-15 08:23:07 -0300

    index

--- projects/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ projects/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -20,7 +20,7 @@ class MarcaController extends Controller
     public function index()
     {
       // $marcas = Marca::all();
-      $marcas = $this->marca->all();
+      $marcas = $this->marca->with('modelos')->get();
       return response()->json($marcas, 200);
 
     }
