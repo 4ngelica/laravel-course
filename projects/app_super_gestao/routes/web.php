205f27344b358c95c5387efa683f90242c245357
Angélica Batassim <angelicabatassim@gmail.com>
2021-08-28 09:55:07 -0300

    Add Produto resource route

--- projects/app_super_gestao/routes/web.php
+++ projects/app_super_gestao/routes/web.php
@@ -30,14 +30,17 @@ use App\Http\Middleware\LogAcessoMiddleware;
       Route::get('/home', 'HomeController@index')->name("app.home");
       Route::get('/sair', 'LoginController@sair')->name("app.sair");
       Route::get('/cliente', 'ClienteController@index')->name("app.cliente");
+      //Fornecedor
       Route::get('/fornecedor', 'FornecedorController@index')->name("app.fornecedor");
       Route::post('/fornecedor/listar', 'FornecedorController@listar')->name("app.fornecedor.listar");
       Route::get('/fornecedor/listar', 'FornecedorController@listar')->name("app.fornecedor.listar");
       Route::get('/fornecedor/adicionar', 'FornecedorController@adicionar')->name("app.fornecedor.adicionar");
       Route::post('/fornecedor/adicionar', 'FornecedorController@adicionar')->name("app.fornecedor.adicionar");
-      Route::get('/produto', 'ProdutoController@index')->name("app.produto");
       Route::get('/fornecedor/editar/{id}/{msg?}', 'FornecedorController@editar')->name("app.fornecedor.editar");
       Route::get('/fornecedor/excluir/{id}', 'FornecedorController@excluir')->name("app.fornecedor.excluir");
+
+      //Produto
+      Route::resource('produto', 'ProdutoController')->name("app.produto");
   });
 
   //Msgs de callback
