139e65ebcbbb0a79a4eda9be277abe46d8fc8468
Angélica Batassim <angelicabatassim@gmail.com>
2021-08-30 10:08:54 -0300

    Refactoring

--- projects/app_super_gestao/resources/views/app/produto/create.blade.php
+++ projects/app_super_gestao/resources/views/app/produto/create.blade.php
@@ -3,7 +3,11 @@
 @section('conteudo')
   <div class="conteudo-pagina">
     <div class="titulo-pagina-2">
-      <p>Adicionar produto</p>
+      @if (isset($produto->id))
+        <p>Editar produto</p>
+      @else
+        <p>Adicionar produto</p>
+      @endif
     </div>
     <div class="menu">
       <u>
@@ -15,18 +19,25 @@
     <div class="informacao-pagina">
       {{-- {{$msg ?? ''}} --}}
       <div style="width:30%; margin-left:auto; margin-right:auto;">
+
+        @if (isset($produto->id))
+        <form class="" action="{{route('produto.update', ['produto' =>$produto->id])}}" method="post">
+          @csrf
+          @method('PUT')
+        @else
         <form class="" action="{{route('produto.store')}}" method="post">
           @csrf
-          <input type="text" name="nome" placeholder="Nome" value="{{old('nome')}}" class="borda-preta">
+        @endif
+          <input type="text" name="nome" placeholder="Nome" value="{{$produto->nome ?? old('nome')}}" class="borda-preta">
           {{ $errors->has('nome') ? $errors->first('nome') : ''}}
-          <input type="text" name="descricao" placeholder="Descrição" value="{{old('descricao')}}" class="borda-preta">
+          <input type="text" name="descricao" placeholder="Descrição" value="{{$produto->descricao ?? old('descricao')}}" class="borda-preta">
           {{$errors->has('descricao') ? $errors->first('descricao'): ''}}
-          <input type="text" name="peso" placeholder="Peso" value="{{old('peso')}}" class="borda-preta">
+          <input type="text" name="peso" placeholder="Peso" value="{{$produto->peso ?? old('peso')}}" class="borda-preta">
           {{$errors->has('peso') ? $errors->first('peso'): ''}}
           <select class="borda-preta" name="unidade_id">
             <option value="">Selecione a unidade de medida</option>
             @foreach ($unidades as $unidade)
-            <option value="{{$unidade->id}}" {{old('unidade_id') == $unidade->id ? 'selected' : ''}}>{{$unidade->descricao}}</option>
+            <option value="{{$unidade->id}}" {{ ($produto->unidade_id ?? old('unidade_id')) == $unidade->id ? 'selected' : ''}}>{{$unidade->descricao}}</option>
             @endforeach
           </select>
           {{$errors->has('unidade_id') ? $errors->first('unidade_id'): ''}}
