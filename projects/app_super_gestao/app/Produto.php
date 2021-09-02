3da41d1a699b58d310a959c37bfae2536ca08f54
Angélica Batassim <angelicabatassim@gmail.com>
2021-09-02 09:41:06 -0300

    Add Product belongsTo (ORM 1:N)

--- projects/app_super_gestao/app/Produto.php
+++ projects/app_super_gestao/app/Produto.php
@@ -12,4 +12,8 @@ class Produto extends Model
   {
     return $this->hasOne('App\ProdutoDetalhe');
   }
+
+  public function fornecedor(){
+    return $this->belongsTo('App\Fornecedor');
+  }
 }
--- projects/app_super_gestao/resources/views/app/produto/create.blade.php
+++ projects/app_super_gestao/resources/views/app/produto/create.blade.php
@@ -14,7 +14,7 @@
     </div>
     <div class="informacao-pagina">
       <div style="width:30%; margin-left:auto; margin-right:auto;">
-        @component('app.produto._components.form_create_edit')
+        @component('app.produto._components.form_create_edit',['unidades'=>$unidades])
         @endcomponent
       </div>
     </div>
