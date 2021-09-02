e47c7294a8e1eadce05c597e45dcd8aed1607dc6
Angélica Batassim <angelicabatassim@gmail.com>
2021-09-02 11:51:50 -0300

    Add PedidoProduto, Cliente and PEdidos structure

--- projects/app_super_gestao/routes/web.php
+++ projects/app_super_gestao/routes/web.php
@@ -29,7 +29,6 @@ use App\Http\Middleware\LogAcessoMiddleware;
     Route::middleware('autenticacao:padrao,visitante,p3,p4')->prefix('/app')->group(function(){
       Route::get('/home', 'HomeController@index')->name("app.home");
       Route::get('/sair', 'LoginController@sair')->name("app.sair");
-      Route::get('/cliente', 'ClienteController@index')->name("app.cliente");
       //Fornecedor
       Route::get('/fornecedor', 'FornecedorController@index')->name("app.fornecedor");
       Route::post('/fornecedor/listar', 'FornecedorController@listar')->name("app.fornecedor.listar");
@@ -44,6 +43,11 @@ use App\Http\Middleware\LogAcessoMiddleware;
 
       //Produtos detalhes
       Route::resource('produto-detalhe', 'ProdutoDetalheController');
+
+      Route::resource('cliente', 'ClienteController');
+      Route::resource('pedido', 'PedidoController');
+      Route::resource('pedido-produto', 'PedidoProdutoController');
+
   });
 
   //Msgs de callback
