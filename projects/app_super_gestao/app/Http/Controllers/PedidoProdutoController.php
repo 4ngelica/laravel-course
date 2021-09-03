a5daebd8d1516af1138a59583a7b960bcc84e533
Angélica Batassim <angelicabatassim@gmail.com>
2021-09-03 14:49:16 -0300

    N:N

--- projects/app_super_gestao/app/Http/Controllers/PedidoProdutoController.php
+++ projects/app_super_gestao/app/Http/Controllers/PedidoProdutoController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Pedido;
 use App\Produto;
+use App\PedidoProduto;
+
 
 class PedidoProdutoController extends Controller
 {
@@ -26,6 +28,7 @@ class PedidoProdutoController extends Controller
     public function create(Pedido $pedido)
     {
         $produtos = Produto::all();
+        $pedido->produtos;
         return view('app.pedido_produto.create', ['pedido'=>$pedido, 'produtos'=>$produtos]);
     }
 
@@ -37,7 +40,18 @@ class PedidoProdutoController extends Controller
      */
     public function store(Request $request, Pedido $pedido)
     {
-      // return redirect()->route();
+      $regras = [
+        'produto_id' => 'exists:produtos,id'
+      ];
+
+      $request->validate($regras);
+      $pedidoProduto = new PedidoProduto;
+      $pedidoProduto->pedido_id = $pedido->id;
+      $pedidoProduto->produto_id = $request->get('produto_id');
+      $pedidoProduto->save();
+
+
+      return redirect()->route('pedido-produto.create',['pedido'=>$pedido->id]);
     }
 
     /**
