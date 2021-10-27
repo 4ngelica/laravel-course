c5469c9ce8b042623220475efdd240a1ebf3a8f6
Angélica <angelica.nunes@usp.br>
2021-10-27 08:19:47 -0300

    Add component props

--- projects/app_locadora_carros/resources/js/components/Login.vue
+++ projects/app_locadora_carros/resources/js/components/Login.vue
@@ -4,9 +4,9 @@
         <div class="col-md-8">
             <div class="card">
                 <div class="card-header">Login</div>
-
                 <div class="card-body">
                     <form method="POST" action="">
+                    <input type="hidden" name="_token" :value="csrf_token">
 
                         <div class="form-group row">
                             <label for="email" class="col-md-4 col-form-label text-md-right">E-mail</label>
@@ -54,3 +54,9 @@
     </div>
 </div>
 </template>
+
+<script>
+  export default {
+    props: ['csrf_token']
+  }
+</script>
