513eced38bdb1371fb2aade99e3c594f64924043
Angélica <angelica.nunes@usp.br>
2021-11-02 15:06:56 -0300

    Add home component

---
+++ projects/app_locadora_carros/resources/js/components/Home.vue
@@ -0,0 +1,15 @@
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Welcome</div>
+
+                <div class="card-body">
+                    <p>Show app metrics</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
