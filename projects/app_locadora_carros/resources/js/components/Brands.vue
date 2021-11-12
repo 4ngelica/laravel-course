810fb91b4724f1db007f9908b3cc43d4517439e2
Angélica <angelica.nunes@usp.br>
2021-11-12 10:02:40 -0300

    Add container-component

--- projects/app_locadora_carros/resources/js/components/Brands.vue
+++ projects/app_locadora_carros/resources/js/components/Brands.vue
@@ -3,18 +3,18 @@
     <div class="row justify-content-center">
       <div class="col-md-8">
         <div class="card">
-          <div class="card-header">Brands</div>
+          <div class="card-header">Brand searching</div>
           <div class="card-body">
             <div class="form-row">
-              <div class=" col form-group">
-                <label for="inputId">Brand searching</label>
-                <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
-                <small id="idHelp" class="form-text text-muted">Optional. Search by the brand ID.</small>
+              <div class="col">
+                <input-container-component title="ID" id="inputId" id-help="IdHelp" help-text="Optional. Search by the brand ID.">
+                  <input type="number" class="form-control" id="inputId" aria-describedby="idHelp" placeholder="ID">
+                </input-container-component>
               </div>
-              <div class="col form-group">
-                <label for="inputName">Name</label>
-                <input type="text" class="form-control" id="inputName" aria-describedby="nameHelp" placeholder="Name">
-                <small id="nameHelp" class="form-text text-muted">Optional. Search by the brand name.</small>
+              <div class="col">
+                <input-container-component title="Name" id="inputName" name-help="NameHelp" help-text="Optional. Search by the brand name.">
+                  <input type="text" class="form-control" id="inputName" aria-describedby="idName" placeholder="Name">
+                </input-container-component>
               </div>
             </div>
           </div>
---
+++ projects/app_locadora_carros/resources/js/components/InputContainer.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class=" col form-group">
+    <label :for="id">{{title}}</label>
+    <slot></slot>
+    <small :id="idHelp" class="form-text text-muted">{{helpText}}</small>
+  </div>
+</template>
+
+<script>
+    export default {
+      props: ['id', 'title', 'helpText', 'idHelp']
+    }
+</script>
